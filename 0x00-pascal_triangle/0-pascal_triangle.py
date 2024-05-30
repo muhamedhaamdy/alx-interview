09de95db1d80cf95f54c936672e38e97c42f3418
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-05-31 02:03:27 +0300

    pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,12 @@
+def pascal_triangle(n):
+    # Initialize each row with the appropriate number of elements
+    c = [[0] * (i + 1) for i in range(5)]
+    for row in range(5):
+        for col in range(row + 1):
+            if not row and not col:
+                c[row][col] = 1
+            elif col == 0 or col == row:
+                c[row][col] = 1
+            else:
+                c[row][col] = c[row - 1][col - 1] + c[row - 1][col]
+    return c
