68fbea94978ba046c527ff497fccbaa06c07f02e
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-06-06 15:14:09 +0300

    fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -23,17 +23,7 @@ def canUnlockAll(boxes):
     while len(stack):
         box = stack.pop()
         for key in box:
-            if not unlocked[key]:
+            if not unlocked[key] and key < len(boxes):
                 stack.append(boxes[key])
                 unlocked[key] = 1
     return True if sum(unlocked) == len(boxes) else False
-
-
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
