a1da29fd1f6c0c91a14a4da18f10fdc6025a1de5
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-05 03:41:17 +0300

    N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,128 +1,10 @@
 #!/usr/bin/python3
 '''nqueens problem'''
 import sys
-# if len(sys.argv) != 2:
-#     print('Usage: nqueens N')
-#     sys.exit(1)
 
-# n = sys.argv[1]
-# if not n.isdigit():
-#     print('N must be a number')
-#     sys.exit(1)
-
-# n = int(n)
-# if n < 4:
-#     print('N must be at least 4')
-#     sys.exit(1)
-
-# n = 6
-
-# sol = []
-# stack = []
-# board = [[0 for _ in range(n)] for _ in range(n)]
-# visited = []
-
-# def copy_arr(array):
-#     return [row[:] for row in array]
-
-# def remove_duplicates(board_list):
-#     seen = set()
-#     unique_boards = []
-#     for board in board_list:
-#         # Convert board to tuple to make it hashable
-#         board_tuple = tuple(tuple(row) for row in board)
-#         if board_tuple not in seen:
-#             unique_boards.append(board)
-#             seen.add(board_tuple)
-#     return unique_boards
-
-# def is_save(row, col, board):
-#     if board[row][col]:
-#         return False
-#     for i in range(0, n):
-#         if board[i][col]:
-#             return False
-#     for i in range(0, n):
-#         if board[row][i]:
-#             return False
-#     r, c = (row, col)
-#     while r < n and c < n:
-#         if board[r][c]:
-#             return False
-#         r += 1
-#         c += 1
-#     r, c = (row, col)
-#     while r >= 0 and c >= 0:
-#         if board[r][c]:
-#             return False
-#         r -= 1
-#         c -= 1
-#     r, c = (row, col)
-#     while r < n and c >= 0:
-#         if board[r][c]:
-#             return False
-#         r += 1
-#         c -= 1
-#     r, c = (row, col)
-#     while r >= 0 and c < n:
-#         if board[r][c]:
-#             return False
-#         r -= 1
-#         c += 1
-
-#     return True
-
-# def knigh_move(row, col, board):
-#     knigh = [(2,1), (2,-1), (-2,1), (-2,-1), (1,2), (1,-2), (-1,2), (-1,-2)]
-#     for (r, c) in knigh:
-#         board_copy = copy_arr(board)
-#         if row + r < n and row + r >= 0 and col + c < n and col + c >= 0 and is_save(row + r, col + c, board):
-#             board_copy[row + r][col + c] = 1
-#             stack.append(board_copy)
-
-# def solved_board(board):
-#     for i in range(n):
-#         for j in range(n):
-#             if board[i][j] == 1:
-#                 if not is_save(i, j, board):
-#                     return False
-#     return True
-
-# def queen_position(board):
-#     queen_pos = []
-#     for i in range(n):
-#         for j in range(n):
-#             if board[i][j] == 1:
-#                 queen_pos.append([i, j])
-#     return queen_pos
-
-# def print_board(board):
-#     for row in board:
-#         print(row)
-#     print()
-
-# if __name__ == '__main__':
-#     for i in range(n):
-#         board[0][i] = 1
-#         stack.append(copy_arr(board))
-#         board[0][i] = 0    
-#     while stack:
-#         board = stack.pop(0)
-#         for i in range(n):
-#             for j in range(n):
-#                 if board[i][j] == 1 and (i, j) not in visited:
-#                     visited.append((i, j))
-#                     knigh_move(i, j, board)
-#                     print_board(board)
-#         if solved_board(board):
-#             sol.append(board)
-#         if sum([sum(row) for row in board]) == n:
-#             sol.append(board)
-#     sol = remove_duplicates(sol)
-#     for board in sol:
-#         print(queen_position(board))
 
 def print_solution(board):
+    '''Prints the solution to the nqueens problem'''
     solution = []
     for row in range(len(board)):
         for col in range(len(board)):
@@ -130,25 +12,26 @@ def print_solution(board):
                 solution.append([row, col])
     print(solution)
 
+
 def is_safe(board, row, col, N):
-    # Check this row on left side
+    '''check if the position is safe for the queen'''
     for i in range(col):
         if board[row][i] == 1:
             return False
 
-    # Check upper diagonal on left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-    # Check lower diagonal on left side
     for i, j in zip(range(row, N, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
     return True
 
+
 def solve_nqueens_util(board, col, N):
+    '''backtracking function'''
     if col >= N:
         print_solution(board)
         return True
@@ -162,13 +45,17 @@ def solve_nqueens_util(board, col, N):
 
     return res
 
+
 def solve_nqueens(N):
+    '''Solves the nqueens problem'''
     board = [[0 for _ in range(N)] for _ in range(N)]
     if not solve_nqueens_util(board, 0, N):
         print("Solution does not exist")
         return
 
+
 if __name__ == "__main__":
+    '''main'''
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
